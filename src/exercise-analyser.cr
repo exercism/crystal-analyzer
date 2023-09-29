91b58de9fbeaa923fa6cab1543c80738b8c54433
meatball <69751659+meatball133@users.noreply.github.com>
2023-09-29 10:45:01 +0200

    [1.0.1]: Various fixes (#5)
    * Various fixes
    
    * Update sieve test
    
    * Fix tag (#6) (#7)
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>
    
    ---------
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>

--- src/exercise-analyser.cr
+++ src/exercise-analyser.cr
@@ -29,14 +29,17 @@ class ExerciseAnayzer
     case exercise
     when "sieve"
       if anlyzation.any? { |x| (x.options["name"] == "%" || x.options["name"] == "/") && x.options["type"] == "Call" }
-        @comments << Comments.new("crystal.sieve.do_not_use_div", Hash(String, String | Int32).new, "essential")
+        @comments << Comments.new("crystal.sieve.do_not_use_div_rem", Hash(String, String | Int32).new, "essential")
       end
     when "two-fer"
-      if anlyzation.none? { |x| x.options["name"] == "you" && x.options["type"] == "Arg" && x.inside_method == "two_fer" }
+      if anlyzation.none? do |x|
+          first_argumment = x.argumments[0]
+            first_argumment["default_argument"]? == "\"you\"" && first_argumment["type"] == "Arg" && x.inside_method == "two_fer"
+         end
         @comments << Comments.new("crystal.two-fer.incorrect_default_param", Hash(String, String | Int32).new, "actionable")
       end
       if anlyzation.any? { |x| x.options["name"] == "+" && x.options["type"] == "Call" && x.inside_method == "two_fer" }
-        @comments << Comments.new("rystal.two-fer.string_concatenation", Hash(String, String | Int32).new, "actionable")
+        @comments << Comments.new("crystal.two-fer.string_concatenation", Hash(String, String | Int32).new, "actionable")
       end
     when "arys-amazing-lasagna"
       if anlyzation.none? { |x| x.options["name"] == "preparation_time_in_minutes" && x.options["type"] == "Call" && x.inside_method == "total_time_in_minutes" }
@@ -46,7 +49,7 @@ class ExerciseAnayzer
         options["item_2"] = "Lasagna.total_time_in_minutes"
         @comments << Comments.new("crystal.general.reuse", options, "actionable")
       end
-      if anlyzation.none? { |x| x.options["name"] == "EXPECTED_MINUTES_IN_OVEN" && x.options["name"] == "Assign" && x.inside_method == "remaining_minutes_in_oven" }
+      if anlyzation.none? { |x| x.options["name"] == "-" && x.options["receiver"]? == "EXPECTED_MINUTES_IN_OVEN" && x.options["type"] == "Call" && x.inside_method == "remaining_minutes_in_oven" }
         options = Hash(String, String | Int32).new
         options["concept"] = "constant"
         options["item_1"] = "Lasagna.remaining_minutes_in_oven"
@@ -55,7 +58,7 @@ class ExerciseAnayzer
       end
     when "navigation-computer"
       if anlyzation.any? do |x|
-           temp = x.options["value"]
+           temp = x.options["value"]?
            unless temp.nil?
              {"NEPTUNE_DISTANCE", "MARS_DISTANCE", "ATMOSPHERE_DISTANCE"}.includes?(x.options["name"]) && x.options["type"] == "Assign" && temp.includes?("to_i")
            end
